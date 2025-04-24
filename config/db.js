e5c600b0b0d5b29bfd8fb05b408089844bb993a6
Saranga Ranasinghe <ranasingherpksb.22@uom.lk>
2025-04-24 15:16:42 +0530

    changes made db.js
    db.js changed

--- config/db.js
+++ config/db.js
@@ -7,9 +7,16 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,  
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_PORT:', process.env.DB_PORT);
 
-module.exports = pool;
+
+module.exports = pool;
