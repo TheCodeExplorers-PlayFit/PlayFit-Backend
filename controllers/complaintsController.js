8e5b2878a83697fb0902b683ffca5cc07ae3d90c
oshanaHQ <oshanarasminda@gmail.com>
2025-07-16 17:23:56 +0530

    updated complaint controller

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2/promise');
-const pool = require('../config/db');
+const { pool } = require('../config/db');
 
 // Get all stadiums for complaint form
 exports.getStadiums = async (req, res) => {
